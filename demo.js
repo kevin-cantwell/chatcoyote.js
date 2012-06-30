138de0831ce5fc96f2fbb17d4e7e4fbe68992f9f
=kevin-cantwell <=kevin.cantwell@gmail.com>
2012-06-30 15:32:38 -0400

    First pages commit

---
+++ demo.js
@@ -0,0 +1,7 @@
+Demo = {
+  init : function () {
+    $(".chatcoyote").eq(0).chatcoyote({ room : 'puppies' });
+    $(".chatcoyote").eq(1).chatcoyote({ room : 'cutepuppies' });
+    $(".chatcoyote").eq(2).chatcoyote({ room : 'uglycats' });
+  }
+};
