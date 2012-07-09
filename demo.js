40b0e0c8a7602502d4bf136d84b1d8b0d9f2214a
=kevin-cantwell <=kevin.cantwell@gmail.com>
2012-07-08 20:25:21 -0400

    Some style changes.

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 Demo = {
   init : function () {
-    $(".chatcoyote").eq(0).chatcoyote({ room : 'puppies' });
-    $(".chatcoyote").eq(1).chatcoyote({ room : 'cutepuppies' });
-    $(".chatcoyote").eq(2).chatcoyote({ room : 'uglycats' });
+    $(".chatcoyote").eq(0).chatcoyote({ room : 'Puppies' });
+    $(".chatcoyote").eq(1).chatcoyote({ room : "Lil' Pups" });
+    $(".chatcoyote").eq(2).chatcoyote({ room : 'Ugly Cats' });
   }
 };
