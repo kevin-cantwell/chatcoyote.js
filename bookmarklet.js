a03f28e9d69429d81b0dd8913e9f1a3e9fe37ca8
=kevin-cantwell <=kevin.cantwell@gmail.com>
2012-07-08 22:53:19 -0400

    First stab at the bookmarklet

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:void(   (function() {     var e = document.createElement('script');     e.setAttribute('type','text/javascript');     e.setAttribute('charset','UTF-8');     e.setAttribute('src','http://localhost:3000/js/chatmarklet.js?cachebuster='+Math.random()*99999999);     document.body.appendChild(e)   })() );
