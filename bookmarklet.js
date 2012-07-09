06030b531a756fe4c0655fb6f6a003d941270f44
=kevin-cantwell <=kevin.cantwell@gmail.com>
2012-07-08 23:48:28 -0400

    updating the bookmarklet and removing borders from the plugin

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,9 @@
-javascript:void(   (function() {     var e = document.createElement('script');     e.setAttribute('type','text/javascript');     e.setAttribute('charset','UTF-8');     e.setAttribute('src','http://localhost:3000/js/chatmarklet.js?cachebuster='+Math.random()*99999999);     document.body.appendChild(e)   })() );
+javascript:void(
+  (function() {
+    var e = document.createElement('script');
+    e.setAttribute('type','text/javascript');
+    e.setAttribute('charset','UTF-8');
+    e.setAttribute('src','http://yacc.meteor.com/js/chatmarklet.js?cachebuster='+Math.random()*99999999);
+    document.body.appendChild(e)
+  })()
+);
